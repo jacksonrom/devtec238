4ae4289054f6f124b2a6c32215a857c4e31db77e
Jackson Romeu <jackson.r.filho@aluno.senai.br>
2024-05-21 10:32:51 -0400

    Update controller.java

--- src/br/tec/jack/controller/controller.java
+++ src/br/tec/jack/controller/controller.java
@@ -1,10 +1,31 @@
-package br.tec.jack.principal;
+// Em um pacote chamado "controller"
+public class Negocio {
+    public String mediaAluno(double nota1, double nota2, double nota3, double nota4) {
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        if (media >= 7.0) {
+            return "Aprovado";
+        } else {
+            return "Reprovado";
+        }
+    }
+}
 
-public class controller {
+// Na classe principal
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite as 4 notas:");
+        double nota1 = scanner.nextDouble();
+        double nota2 = scanner.nextDouble();
+        double nota3 = scanner.nextDouble();
+        double nota4 = scanner.nextDouble();
+        
+        Negocio negocio = new Negocio();
+        String resultado = negocio.mediaAluno(nota1, nota2, nota3, nota4);
+        
+        System.out.println("Resultado: " + resultado);
+    }
 }
